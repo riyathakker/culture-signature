a47866bfd90f68a6885bf360369d5c4399b72052
Riya thakker <riyathakker@o2h.com>
2026-08-17 17:20:57 +0530

    fix: hide install app

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: true, //update when site goes live
   workboxOptions: {
     disableDevLogs: true,
   },
